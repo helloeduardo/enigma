7a2600bfa201b8894c94fcb60069233a474e2a96
Eduardo Parra <helloeduardo@icloud.com>
2020-09-19 17:16:38 -0600

    Add cipher object attribute and test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,15 +2,20 @@ require './lib/defaultable'
 
 class Enigma
   include Defaultable
+  attr_reader :cipher
+
+  def initialize
+    @cipher = EnigmaCipher.new
+  end
 
   def encrypt(message, key = default_key, date = default_date)
-    { encryption: nil, #encrypted message method
+    { encryption: cipher.encrypt(message, key, date),
       key: key,
       date: date }
   end
 
   def decrypt(ciphertext, key, date = default_date)
-    { decryption: nil, #decrypted message method
+    { decryption: cipher.decrypt(ciphertext, key, date),
       key: key,
       date: date }
   end
