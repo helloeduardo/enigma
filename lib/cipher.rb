c3f082dbc0cfa3a9d436a307b42979f926b2fa5a
Eduardo Parra <helloeduardo@icloud.com>
2020-09-19 11:55:19 -0600

    Add character set and test

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,3 +1,7 @@
 class Cipher
-  
+  attr_reader :char_set
+
+  def initialize
+    @char_set = ("a".."z").to_a << " "
+  end
 end
--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -9,4 +9,16 @@ class CipherTest < Minitest::Test
     assert_instance_of Cipher, cipher
   end
 
+  def test_it_has_a_character_set
+    cipher = Cipher.new
+
+    expected = [
+      "a", "b", "c", "d", "e", "f", "g", "h", "i",
+      "j", "k", "l", "m", "n", "o", "p", "q", "r",
+      "s", "t", "u", "v", "w", "x", "y", "z", " "
+    ]
+
+    assert_equal expected, cipher.char_set
+  end
+
 end
