6862c2e3714a35556879e948e5b8d20f70947049
Eduardo Parra <helloeduardo@icloud.com>
2020-09-19 10:54:25 -0600

    Add encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,5 +3,9 @@ require './lib/defaultable'
 class Enigma
   include Defaultable
 
-  
+  def encrypt(message, key, date)
+    { encryption: nil, #encrypted message method
+      key: key,
+      date: date }
+  end
 end
