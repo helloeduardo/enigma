26a3754ce1e968e8fff31e2626816db8bed2fb5e
Eduardo Parra <helloeduardo@icloud.com>
2020-09-20 10:47:08 -0600

    Add attributes and test

--- lib/enigma_bombe.rb
+++ lib/enigma_bombe.rb
@@ -1,3 +1,8 @@
 class EnigmaBombe
-  
+  attr_reader :char_set, :known_ending
+
+  def initialize
+    @char_set = ("a".."z").to_a << " "
+    @known_ending = " end"
+  end
 end
--- test/enigma_bombe_test.rb
+++ test/enigma_bombe_test.rb
@@ -8,4 +8,22 @@ class EnigmaBombeTest < Minitest::Test
 
     assert_instance_of EnigmaBombe, bombe
   end
+
+  def test_it_has_a_character_set
+    bombe = EnigmaBombe.new
+
+    expected = [
+      "a", "b", "c", "d", "e", "f", "g", "h", "i",
+      "j", "k", "l", "m", "n", "o", "p", "q", "r",
+      "s", "t", "u", "v", "w", "x", "y", "z", " "
+    ]
+
+    assert_equal expected, bombe.char_set
+  end
+
+  def test_it_has_a_known_ending
+    bombe = EnigmaBombe.new
+
+    assert_equal " end", bombe.known_ending
+  end
 end
