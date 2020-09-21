e3c35a4d97d956ac4967fb7266f2f2d6553271c9
Eduardo Parra <helloeduardo@icloud.com>
2020-09-21 12:25:45 -0600

    Reorder tests to match class

--- test/enigma_cipher_test.rb
+++ test/enigma_cipher_test.rb
@@ -22,10 +22,34 @@ class EnigmaCipherTest < Minitest::Test
   end
 
   def test_it_can_set_cipher
-    @cipher.set_cipher('hello world', '02715', '040895')
     assert_equal 'hello world', @cipher.message
     assert_equal '02715', @cipher.key
     assert_equal '040895', @cipher.date
+  end
+
+  def test_it_can_encrypt_with_key_and_date
+    expected = 'keder ohulw'
+
+    assert_equal expected, @cipher.encrypt('hello world', '02715', '040895')
+  end
+
+  def test_it_can_decrypt_with_key_and_date
+    @cipher.stubs(:message).returns('keder ohulw')
+
+    expected = 'hello world'
+
+    assert_equal expected, @cipher.decrypt('keder ohulw', '02715', '040895')
+  end
+
+  def test_it_can_find_shifts
+    expected = {
+      a: 3,
+      b: 27,
+      c: 73,
+      d: 20
+    }
+
+    assert_equal expected, @cipher.shifts
   end
 
   def test_it_can_find_keys
@@ -50,32 +74,7 @@ class EnigmaCipherTest < Minitest::Test
     assert_equal expected, @cipher.offsets
   end
 
-  def test_it_can_find_shifts
-    expected = {
-      a: 3,
-      b: 27,
-      c: 73,
-      d: 20
-    }
-
-    assert_equal expected, @cipher.shifts
-  end
-
-  def test_it_can_encrypt_with_key_and_date
-    expected = 'keder ohulw'
-
-    assert_equal expected, @cipher.encrypt('hello world', '02715', '040895')
-  end
-
-  def test_it_can_decrypt_with_key_and_date
-    @cipher.stubs(:message).returns('keder ohulw')
-
-    expected = 'hello world'
-
-    assert_equal expected, @cipher.decrypt('keder ohulw', '02715', '040895')
-  end
-
-  def test_it_can_crypt
+  def test_it_can_translate
     shifts = [3, 27, 73, 20]
 
     expected = 'keder ohulw'
