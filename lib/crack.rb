70981dbad08f1deea99bff9d589d47bf811f74e9
Eduardo Parra <helloeduardo@icloud.com>
2020-09-20 21:02:56 -0600

    Add crack interface

---
+++ lib/crack.rb
@@ -0,0 +1,12 @@
+require './lib/enigma'
+require './lib/enigma_cipher'
+require './lib/enigma_bombe'
+
+message = File.read(ARGV[0]).chomp
+
+enigma = Enigma.new
+plaintext = enigma.crack(message, ARGV[2])
+
+File.open(ARGV[1], "w").write(plaintext[:decryption])
+
+p "Created '#{ARGV[1]}' with the key #{plaintext[:key]} and date #{plaintext[:date]}"
